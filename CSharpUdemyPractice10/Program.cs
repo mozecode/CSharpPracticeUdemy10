609a4863d6ca2f446fdd238f9d8f9ecb17b850c4
Greta Moseley <greta.moseley@gmail.com>
2017-12-20 16:55:35 -0600

    add C# exercise dealing with turning a string into a character array, reversing the array, and converting it back into a string.

--- CSharpUdemyPractice10/Program.cs
+++ CSharpUdemyPractice10/Program.cs
@@ -10,6 +10,35 @@ namespace CSharpUdemyPractice10
     {
         static void Main(string[] args)
         {
+            //Write a program and ask the user to enter their name. Use an array to reverse the name and then 
+            //store the result in a new string. Display the reversed name on the console.
+
+            Console.WriteLine("Please enter your name: ");
+            var name = Console.ReadLine();
+
+            var nameCharArray = name.ToCharArray();
+            Array.Reverse(nameCharArray); //split into an array of letters & reverse?
+
+           //convert back into string using new keyword (ToString won't work)
+
+            var stringified= new string(nameCharArray);
+
+
+            Console.WriteLine(stringified);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
